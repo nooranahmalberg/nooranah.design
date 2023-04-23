f2fe8282f0d1b1ee0fa29a1fc8831be2d73964b5
Nooranah Malberg <ranah.malberg@gmail.com>
2023-04-23 15:06:59 +1000

    first draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     colors: {
+      current: "currentColor",
       marshmallow: "hsl(45, 100%, 97%)",
       liquorice: "hsl(0, 4%, 11%)",
-      mango: "hsl(11, 80%, 60%)",
+      mango: "hsl(61, 100%, 74%)",
     },
     fontFamily: {
       sans: ["Quicksand", "sans-serif"],

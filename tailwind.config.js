f5a29dace20cf668d8b5f6a28ea5c128378dd008
Nooranah Malberg <ranah.malberg@gmail.com>
2023-05-16 16:06:37 +1000

    Mobile experience done and fixed stuff
    Home page complete. Next: images and writing in Projects section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
       marshmallow: "hsl(45, 100%, 97%)",
       liquorice: "hsl(0, 4%, 11%)",
       mango: "hsl(61, 100%, 74%)",
+      berry: "hsl(313, 70%, 76%)",
+      berrypop: "hsl(313, 70%, 91%)",
     },
     fontFamily: {
       Roobert: ["Roobert TRIAL", "sans-serif"],

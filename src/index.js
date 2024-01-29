a2b9e53008d51cd1a5c6950acc573d4725d3c2c9
Nooranah Malberg <ranah.malberg@gmail.com>
2024-01-29 20:47:38 +1000

    updaded grid

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const bentoProjects = document.querySelectorAll(".bento-project");
+const bentoCards = document.querySelectorAll(".bento-card");
+const bentoButtons = document.querySelectorAll(".bento-button");
+
+const opacityClass = "opacity-50";
+
+function isTouchDevice() {
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
+}
+
+if (!isTouchDevice()) {
+  bentoProjects.forEach((node) => {
+    node.addEventListener("mouseenter", (event) => {
+      bentoProjects.forEach((node) => {
+        if (node !== event.target) node.classList.add(opacityClass);
+      });
+      bentoCards.forEach((node) => node.classList.add(opacityClass));
+      bentoButtons.forEach((node) => node.classList.add(opacityClass));
+    });
+    node.addEventListener("mouseleave", () => {
+      [...bentoProjects, ...bentoCards, ...bentoButtons].forEach((node) =>
+        node.classList.remove(opacityClass)
+      );
+    });
+  });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,14 @@ module.exports = {
       current: "currentColor",
       snow: 'hsl(45, 30%, 99%)',
       mushroom: 'hsl(42, 20%, 85%)',
-      marshmallow: "hsl(45, 21%, 92%)",
-      liquorice: "hsl(157, 6%, 16%)",
+      marshmallow: "hsl(45, 21%, 93%)",
+      showcaseYellow: "hsl(48, 92%, 91%)",
+      showcasePink: "hsl(340, 73%, 91%)",
+      showcaseGreen: "hsl(120, 78%, 91%)",
+      deepmoss: "hsl(156, 10%, 19%)",
       moss: "hsl(157, 6%, 36%)",
-      yellow: "hsl(37, 96%, 90%)",
+      teal100: "hsl(161, 50%, 89%)",
+      teal200: "hsl(161, 50%, 67%)",
       berry: "hsl(313, 70%, 76%)",
       berrypop: "hsl(313, 70%, 91%)",
       berrydeep: "hsl(313,70%,85%)",
@@ -36,6 +40,7 @@ module.exports = {
     fontFamily: {
       sans: ["Roobert", "sans-serif"],
     },
+
   },
   plugins: [],
 };

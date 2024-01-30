54497041308a9d89add16eecc8d71a88f265c245
Nooranah Malberg <ranah.malberg@gmail.com>
2024-01-30 23:48:51 +1000

    updated colours

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const bentoProjects = document.querySelectorAll(".bento-project");
 const bentoCards = document.querySelectorAll(".bento-card");
 const bentoButtons = document.querySelectorAll(".bento-button");
 
-const opacityClass = "opacity-50";
+const opacityClass = "opacity-40";
 
 function isTouchDevice() {
   return (
@@ -18,8 +18,6 @@ if (!isTouchDevice()) {
       bentoProjects.forEach((node) => {
         if (node !== event.target) node.classList.add(opacityClass);
       });
-      bentoCards.forEach((node) => node.classList.add(opacityClass));
-      bentoButtons.forEach((node) => node.classList.add(opacityClass));
     });
     node.addEventListener("mouseleave", () => {
       [...bentoProjects, ...bentoCards, ...bentoButtons].forEach((node) =>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,42 +5,41 @@ module.exports = {
     container: {
       center: true,
       screens: {
-        DEFAULT: '1400px',
+        DEFAULT: "1400px",
         // sm: '640px',
         // md: '768px',
         // lg: '1024px',
         // xl: '1400px',
-      //  ['2xl']: '1536px',
+        //  ['2xl']: '1536px',
       },
       padding: {
-        DEFAULT: '16px',
-        md: '64px',
-      }
+        DEFAULT: "16px",
+        md: "64px",
+      },
     },
     colors: {
       current: "currentColor",
-      snow: 'hsl(45, 30%, 99%)',
-      mushroom: 'hsl(42, 20%, 85%)',
+      snow: "hsl(45, 30%, 99%)",
       marshmallow: "hsl(45, 21%, 93%)",
-      showcaseYellow: "hsl(48, 92%, 91%)",
-      showcasePink: "hsl(340, 73%, 91%)",
-      showcaseGreen: "hsl(120, 78%, 91%)",
+      mushroom: "hsl(42, 20%, 85%)",
+      ground: "hsl(42, 20%, 75%)",
+      heroYellow: "hsl(48, 92%, 91%)",
+      heroPink: "hsl(340, 73%, 91%)",
+      heroGreen: "hsl(120, 78%, 91%)",
       deepmoss: "hsl(156, 10%, 19%)",
       moss: "hsl(157, 6%, 36%)",
-      teal100: "hsl(161, 50%, 89%)",
-      teal200: "hsl(161, 50%, 67%)",
-      berry: "hsl(313, 70%, 76%)",
-      berrypop: "hsl(313, 70%, 91%)",
-      berrydeep: "hsl(313,70%,85%)",
+      teal: "hsl(161, 50%, 67%)",
 
       // dark
-      fern: 'hsl(156, 5%, 18%)',
-      ivy: 'hsl(158, 6%, 26%)'
+      fern: "hsl(156, 5%, 18%)",
+      ivy: "hsl(158, 6%, 26%)",
+      antiheroYellow: "hsl(38, 80%, 70%)",
+      antiheroPink: "hsl(353, 48%, 57%)",
+      antiheroGreen: "hsl(117, 10%, 50%)",
     },
     fontFamily: {
       sans: ["Roobert", "sans-serif"],
     },
-
   },
   plugins: [],
 };
